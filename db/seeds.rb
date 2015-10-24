99a63f8a6c33749d40a56fafcaef50762feeca52
farquhar86 <farquharbradley@gmail.com>
2015-10-23 18:57:24 -0700

    changed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,77 +1,74 @@
-# Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "pristine", price: 300)
-# Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "busted", price: 200)
-# Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "fair", price: 250)
-# Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 16, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 16, condition: "pristine", price: 300)
 
-Product.destroy_all
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "pristine", price: 300)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "busted", price: 200)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "fair", price: 250)
+Product.create(model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "pristine", price: 300)
 
-Product.create([
-	{model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 6', carrier: "ATT", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 6', carrier: "Verizon", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 6', carrier: "T-Mobile", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "fair", price: 250,
-	{model: 'iPhone 6', carrier: "Sprint", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 6', carrier: "Unlocked", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 6', carrier: "Other Carrier", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 5', carrier: "ATT", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 5', carrier: "Verizon", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 5', carrier: "T-Mobile", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "fair", price: 250,
-	{model: 'iPhone 5', carrier: "Sprint", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 5', carrier: "Unlocked", capacity: 32, condition: "pristine", price: 300},
-	{model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "busted", price: 200},
-	{model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "fair", price: 250},
-	{model: 'iPhone 5', carrier: "Other Carrier", capacity: 32, condition: "pristine", price: 300}
-])
