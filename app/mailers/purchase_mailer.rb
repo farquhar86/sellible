47cc7f740e730eceb233a09a2999421eb72df993
farquhar86 <farquharbradley@gmail.com>
2015-10-26 20:38:17 -0700

    emails are working

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,12 +1,12 @@
 class PurchaseMailer < ActionMailer::Base
 layout 'purchase_mailer'
 
- default from: "One Month Commerce <commerce@onemonth.com>"
+ default from: "Sellible <support@sellible.com>"
 
  def purchase_receipt purchase
  @purchase = purchase
 
- mail to: purchase.email, subject: "Thanks for your purchase!"
+ mail to: purchase.email, subject: "Thank you!"
 
   end
 
